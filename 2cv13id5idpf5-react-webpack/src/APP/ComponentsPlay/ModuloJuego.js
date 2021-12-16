3510cb954289876830bbb046362f6facebaa1163
David Perez Antonio <david.perez.antonio51@gmail.com>
2021-12-16 03:52:51 -0600

    Mejoras en el laboratorio virtual

--- 2cv13id5idpf5-react-webpack/src/APP/ComponentsPlay/ModuloJuego.js
+++ 2cv13id5idpf5-react-webpack/src/APP/ComponentsPlay/ModuloJuego.js
@@ -4,7 +4,7 @@ import ModuloMano from './ModuloMano.js';
 import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import Cat from '../assets/Cat.gif';
@@ -17,24 +17,37 @@ class ModuloJuego extends Component {
             ValorD: 1,
             RespuestaActual: 1,
             Color: "fondodav",
-            ControlFlag: true, 
+            WhiteControlFlag: true,
             contador: 0,
-            minutos: 0,            
+            minutos: 0,
+            EndGameFlag: false,
+            SurrenderFlag: false,
         }
         this.updateIzquierdo = this.updateIzquierdo.bind(this);
         this.updateDerecho = this.updateDerecho.bind(this);
         this.MySwal = withReactContent(Swal);
+        this.handlerSurrender = this.handlerSurrender.bind(this);
     }
 
-    componentDidMount(){
-        this.micontador= setInterval(() =>{
+    componentDidMount() {
+        const pregunta = window.location.search
+        fetch('http://localhost:8080/2CV13ID5IDPF5/API/getQuestion' + pregunta)
+            .then(result => {
+                return result.ok ? result.json() : result.status
+            })
+            .then(data => {
+                this.audio = new Audio("http:\\\\localhost:8080" + data.Radar)
+                console.log(this.audio.volume)
+                this.audio.volume = 0
+            })
+        this.micontador = setInterval(() => {
             this.setState({
-                contador: this.state.contador +=1,
-            })            
-        },1000); 
+                contador: this.state.contador += 1,
+            })
+        }, 1000);
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         clearInterval(this.micontador);
     }
 
@@ -50,16 +63,17 @@ class ModuloJuego extends Component {
     }
     componentDidUpdate(prevPops, prevState) {
         var RespuestaPredecida
-
-        if(this.state.contador==60){
+        this.audio.play()
+        console.log("1 actualizacion")
+        if (this.state.contador == 60) {
             this.setState({
                 contador: 0,
-                minutos: this.state.minutos +=1, 
-        })
-    }
+                minutos: this.state.minutos += 1,
+            })
+        }
 
         if (this.state.ValorI != prevState.ValorI) {
-            RespuestaPredecida= (this.state.ValorD * this.props.VIC) / this.props.VDC
+            RespuestaPredecida = (this.state.ValorD * this.props.VIC) / this.props.VDC
             this.setState({
                 RespuestaActual: this.state.ValorI / this.state.ValorD,
             })
@@ -67,19 +81,24 @@ class ModuloJuego extends Component {
                 this.setState({
                     Color: "fondocasi1",
                 })
+                this.audio.volume = 1
             } else if (Math.abs(RespuestaPredecida - this.state.ValorI) == 2) {
                 this.setState({
                     Color: "fondocasi2",
                 })
+                this.audio.volume = 0.05
             } else if (Math.abs(RespuestaPredecida - this.state.ValorI) == 3) {
                 this.setState({
                     Color: "fondocasi3",
-                    ControlFlag: true,
+                    WhiteControlFlag: true,
                 })
+                this.audio.volume = 0.005
+            } else {
+                this.audio.volume = 0
             }
         }
         if (this.state.ValorD != prevState.ValorD) {
-                RespuestaPredecida = (this.state.ValorI * this.props.VDC) / this.props.VIC
+            RespuestaPredecida = (this.state.ValorI * this.props.VDC) / this.props.VIC
             this.setState({
                 RespuestaActual: this.state.ValorI / this.state.ValorD,
             })
@@ -87,39 +106,82 @@ class ModuloJuego extends Component {
                 this.setState({
                     Color: "fondocasi1",
                 })
+                this.audio.volume = 1
             } else if (Math.abs(RespuestaPredecida - this.state.ValorD) == 2) {
                 this.setState({
                     Color: "fondocasi2",
                 })
+                this.audio.volume = 0.05
             } else if (Math.abs(RespuestaPredecida - this.state.ValorD) == 3) {
                 this.setState({
                     Color: "fondocasi3",
-                    ControlFlag: true,
+                    WhiteControlFlag: true,
                 })
+                this.audio.volume = 0.005
+            } else {
+                this.audio.volume = 0
             }
         }
-        if (this.props.RespuestaCorrecta == this.state.RespuestaActual && prevState.RespuestaActual!=this.state.RespuestaActual) {
+        if (this.props.RespuestaCorrecta == this.state.RespuestaActual && prevState.RespuestaActual != this.state.RespuestaActual) {
             this.MySwal.fire({
-                title:"¡Felicidades!",
-                text:"Tiempo Total = "+"Segundos",
+                title: "¡Felicidades eres todo un Crack!",
+                text: "Tiempo Total = " + this.state.minutos + " Minutos y " + this.state.contador + " Segundos",
                 background: '#212529',
                 color: '#716add',
-                backdrop: 'rgba(72, 132, 255, 0.55) url('+Cat+') right top no-repeat',
+                backdrop: 'rgba(72, 132, 255, 0.55) url(' + Cat + ') right top no-repeat',
             })
+            this.audio.pause()
+            clearInterval(this.micontador)
             this.setState({
                 Color: "fondotrue",
+                EndGameFlag: true
             })
         } else if (this.props.RespuestaCorrecta != this.state.RespuestaActual) {
 
             /*-------------------------------------------------------------------------------*/
-            if (this.state.ControlFlag) {
+            if (this.state.WhiteControlFlag) {
                 this.setState({
                     Color: "fondodav",
-                    ControlFlag: false
+                    WhiteControlFlag: false
                 })
             }
         }
 
+    }
+    handlerSurrender() {
+        this.MySwal.fire({
+            background: '#212529',
+            icon: 'warning',
+            iconColor: '#dc3545',
+            text: 'Descuida, lo hiciste bien',
+            title: '¿De veradad deseas Rendirte?',
+            showDenyButton: true,
+            confirmButtonColor: '#dc3545',
+            confirmButtonText: '¡Rendirse!',
+            denyButtonColor: '#198754',
+            denymButtonText: '¡NO!',
+        })
+            .then(result => {
+                console.log(result);
+                if (result.isConfirmed) {
+                    this.MySwal.fire({
+                        background: '#212529',
+                        icon: 'success',//cambiar por custom
+                        iconColor: '#198754',
+                        title: '¿Olvidaste tu valor en Casa?',
+                        text: 'Puedo esperarte mientras vas a buscarlo',
+                        timer: 4500,
+                        didOpen: () => {
+                            this.MySwal.showLoading();
+                        },
+                        willClose: () => {
+                            this.setState({
+                                SurrenderFlag: true
+                            })
+                        }
+                    })
+                }
+            })
     }
 
     render() {
@@ -137,14 +199,18 @@ class ModuloJuego extends Component {
                             <Row>
                                 <Col>
                                     <Stack gap={3}>
-                                        <Button as={Link} to="/2CV13ID5IDPF5/home" size="lg" variant="outline-light">Menu Principal </Button>
+                                        {this.state.EndGameFlag
+                                            ? <Button as={Link} to="/2CV13ID5IDPF5/home" size="lg" variant="outline-light"> Volver al Menú</Button>
+                                            : <Button onClick={this.handlerSurrender} size="lg" variant="outline-light">¿Deseas Rendirte? </Button>}
                                         <Form.Label>Tu tiempo: {this.state.minutos} minutos y {this.state.contador} segundos</Form.Label>
+                                        {this.state.SurrenderFlag ? <Redirect to="/2CV13ID5IDPF5/home" /> : ""}
                                     </Stack>
                                 </Col>
                             </Row>
                         </Container>
-                    </Col>                   
+                    </Col>
                 </Row>
+
             </Container>
         );
     }
